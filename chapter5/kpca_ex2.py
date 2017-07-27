74c5f25091567895adde7e488f9c3a9087b37c4d
Kkalla <dkswns333@gmail.com>
2017-07-27 22:54:33 +0900

    Complete chapter5

--- chapter5/kpca_ex2.py
+++ chapter5/kpca_ex2.py
@@ -1,26 +1,21 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jul 27 16:23:38 2017
+Created on Thu Jul 27 21:56:55 2017
 
-@author: Ajou
+@author: user
 """
-###############################################################################
-## Example using rbf kernel pca
-###############################################################################
-
-## Example 1 - separating half-moon shapes
-
+## Example 2 - separating concentric circles
 import matplotlib.pyplot as plt
-from sklearn.datasets import make_moons
-
-X, y = make_moons(n_samples=100, random_state=123)
+from sklearn.datasets import make_circles
+X, y = make_circles(n_samples=1000, random_state=123, noise=0.1, factor=0.2)
 plt.scatter(X[y==0,0],X[y==0,1], color='r',marker='^',alpha=0.5)
 plt.scatter(X[y==1,0],X[y==1,1], color='b',marker='o',alpha=0.5)
 plt.show()
 
-## standard pca
-import numpy as np
+
+## Standard PCA
 from sklearn.decomposition import PCA
+import numpy as np
 scikit_pca = PCA(n_components=2)
 X_spca = scikit_pca.fit_transform(X)
 fig, ax = plt.subplots(nrows=1,ncols=2, figsize=(7,3))
@@ -28,9 +23,9 @@ ax[0].scatter(X_spca[y==0, 0], X_spca[y==0, 1],
   color='red', marker='^', alpha=0.5)
 ax[0].scatter(X_spca[y==1, 0], X_spca[y==1, 1],
   color='blue', marker='o', alpha=0.5)
-ax[1].scatter(X_spca[y==0, 0], np.zeros((50,1))+0.02,
+ax[1].scatter(X_spca[y==0, 0], np.zeros((500,1))+0.02,
   color='red', marker='^', alpha=0.5)
-ax[1].scatter(X_spca[y==1, 0], np.zeros((50,1))-0.02,
+ax[1].scatter(X_spca[y==1, 0], np.zeros((500,1))-0.02,
   color='blue', marker='o', alpha=0.5)
 ax[0].set_xlabel('PC1')
 ax[0].set_ylabel('PC2')
@@ -39,19 +34,18 @@ ax[1].set_yticks([])
 ax[1].set_xlabel('PC1')
 plt.show()
 
-## rbf kernel pca
+## RBF kernel PCA
+import RBFkernelPCA as rkp
 from matplotlib.ticker import FormatStrFormatter
-import RBFkernelPCA as kp
-
-X_kpca = kp.rbf_kernel_pca(X, gamma=15, n_components=2)
+X_kpca = rkp.rbf_kernel_pca(X, gamma=15, n_components=2)
 fig, ax = plt.subplots(nrows=1,ncols=2, figsize=(7,3))
 ax[0].scatter(X_kpca[y==0, 0], X_kpca[y==0, 1],
   color='red', marker='^', alpha=0.5)
 ax[0].scatter(X_kpca[y==1, 0], X_kpca[y==1, 1],
   color='blue', marker='o', alpha=0.5)
-ax[1].scatter(X_kpca[y==0, 0], np.zeros((50,1))+0.02,
+ax[1].scatter(X_kpca[y==0, 0], np.zeros((500,1))+0.02,
   color='red', marker='^', alpha=0.5)
-ax[1].scatter(X_kpca[y==1, 0], np.zeros((50,1))-0.02,
+ax[1].scatter(X_kpca[y==1, 0], np.zeros((500,1))-0.02,
   color='blue', marker='o', alpha=0.5)
 ax[0].set_xlabel('PC1')
 ax[0].set_ylabel('PC2')
@@ -60,13 +54,4 @@ ax[1].set_yticks([])
 ax[1].set_xlabel('PC1')
 ax[0].xaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
 ax[1].xaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
-plt.show()
-
-
-## Example 2 - separating concentric circles
-
-from sklearn.datasets import make_circles
-X, y = make_circles(n_samples=1000, random_state=123, noise=0.1, factor=0.2)
-plt.scatter(X[y==0,0],X[y==0,1], color='r',marker='^',alpha=0.5)
-plt.scatter(X[y==1,0],X[y==1,1], color='b',marker='o',alpha=0.5)
 plt.show()
